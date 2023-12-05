28b942992a3bd0dc497c0fb886f3df0c6cd878d5
ANkulagin <ankulskull@gmail.com>
2023-12-05 17:00:46 +0300

    create layout

--- user.go
+++ user.go
@@ -7,5 +7,5 @@ type User struct {
 	Id       int    `json:"-" db:"id"`                   // Идентификатор пользователя (не отображается в JSON и используется в базе данных).
 	Name     string `json:"name" binding:"required"`     // Имя пользователя.
 	Username string `json:"username" binding:"required"` // Имя пользователя (логин).
-	Password string `json:"password" binding:"required"` // Пароль пользователя.
+	Password string `json:"password" binding:"required"` //  Пароль пользователя.
 }
